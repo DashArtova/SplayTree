8c1f5b90ee040cda2e384b9b25ca49757eb2f4cc
DashaVolochai <d.artova@ukr.net>
2018-02-08 12:22:46 +0200

    fix

--- SplayTreeProba2.cpp
+++ SplayTreeProba2.cpp
@@ -1,9 +1,6 @@
 // SplayTreeProba2.cpp: определяет точку входа для консольного приложения.
 //
 
-#include "stdafx.h"
-#include<stdio.h>
-#include<stdlib.h>
 #include <iostream>
 
 using namespace std;
@@ -15,12 +12,12 @@ struct node{
 
 
 node* newNode(int number){
-	node* temp = new node;
+	auto temp = new node;
 	temp->value = number;
-	temp->l = temp->r = NULL;
+	temp->l = temp->r = nullptr;
 	return temp;
 }
-
+//Лівий поворот
 node *Zag(node *root)
 {
 	node *temp = root->l;
@@ -28,7 +25,7 @@ node *Zag(node *root)
 	temp->r = root;
 	return temp;
 }
-
+//Правий поворот
 node *Zig(node *root){
 	node *temp = root->r;
 	root->r = temp->l;
@@ -37,38 +34,39 @@ node *Zig(node *root){
 }
 
 node *Splay(node *root, int number){
-	if (root == NULL || root->value == number)
+	if (root == nullptr || root->value == number)
 		return root;
 	if (root->value > number){
-		if (root->l == NULL)
+		if (root->l == nullptr)
 			return root;
 		if (root->l->value > number){
 			root->l->l = Splay(root->l->l, number);
 			root = Zag(root);
 		}
-		else if (root->l->value < number){ // Zig-Zag (Left Right)
-			// First recursively bring the key as root of left-right
+		else if (root->l->value < number){ 
 			root->l->r = Splay(root->l->r, number);
-			// Do first rotation for root->left
-			if (root->l->r != NULL)
+			if (root->l->r != nullptr)
 				root->l = Zig(root->l);
 		}
-		// Do second rotation for root
-		return (root->l == NULL) ? root : Zag(root);
+		if (root->l == nullptr) 
+            return root; 
+        else return Zag(root);
 	}
 	else {
-		if (root->r == NULL)
+		if (root->r == nullptr)
 			return root;
 		if (root->r->value > number){
 			root->r->l = Splay(root->r->l, number);
-			if (root->r->l != NULL)
+			if (root->r->l != nullptr)
 				root->r = Zag(root->r);
 		}
 		else if (root->r->value < number){
 			root->r->r = Splay(root->r->r, number);
 			root = Zig(root);
 		}
-		return (root->r == NULL) ? root : Zig(root);
+		if (root->r == nullptr) 
+            return root;
+        else return Zig(root);
 	}
 }
 
@@ -79,7 +77,7 @@ node *Search(node *root, int number)
 
 void Show(node *root)
 {
-	if (root != NULL){
+	if (root != nullptr){
 		cout << root->value << " ";
 		Show(root->l);
 		Show(root->r);
@@ -87,7 +85,7 @@ void Show(node *root)
 }
 
 node *Insert(node *root, int number){
-	if (root == NULL) 
+	if (root == nullptr) 
 		return newNode(number);
 	root = Splay(root, number);
 	if (root->value == number)
@@ -96,24 +94,24 @@ node *Insert(node *root, int number){
 	if (root->value > number){
 		newnode->r = root;
 		newnode->l = root->l;
-		root->l = NULL;
+		root->l = nullptr;
 	}
 	else{
 		newnode->l = root;
 		newnode->r = root->r;
-		root->r = NULL;
+		root->r = nullptr;
 	}
 	return newnode; 
 }
 
 node *Delete(node *root, int number){
 	node *temp;
-	if (root == NULL)
-		return NULL;
+	if (root == nullptr)
+		return root;
 	root = Splay(root, number);
 	if (number != root->value)
 		return root;
-	if (root->l == NULL){
+	if (root->l == nullptr){
 		temp = root;
 		root = root->r;
 	}
@@ -127,10 +125,9 @@ node *Delete(node *root, int number){
 
 }
 
-
 int main()
 {
-	struct node *root = newNode(10);
+	auto root = newNode(10);
 
 	root = Insert(root, 16);
 	root = Insert(root, 21);
@@ -139,21 +136,20 @@ int main()
 	root = Insert(root, 28);
 
 	root = Search(root, 20);
-	cout << "The Splay tree is " << endl;
+	cout << "Розширюване дерево: " << endl;
 	Show(root);
 	cout << endl;
 
 	int key = 5;
 	root = Delete(root, key);
-	cout << "The modified Splay tree after removing an element with a value 4 is " << endl;
+	cout << "Розширюване дерево після видалення елемента зі значенням 5: " << endl;
 	Show(root);
 	cout << endl;
 
 	root = Insert(root, 25);
-	cout << "The modified Splay tree after inserting an element with a value 25 is " << endl;
+	cout << "Розширюване дерево після додавання елемента зі значенням 25: " << endl;
 	Show(root);
 	cout << endl;
 
-	system("pause");
 	return 0;
 }
